012b56d9ae1bc66aaf4772fa1781e177715c4932
Nelson Rosales <nelson.rosales83@gmail.com>
2023-08-22 19:49:29 +0200

    feat: controllers and first route

---
+++ app/Http/Controllers/DepartmentController.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Department;
+
+class DepartmentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        // display all departments from el_salvador database
+       return Department::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        // create a department in el_salvador database
+        $request->validate([
+            'name' => 'required',
+            'capital' => 'required',
+            'area' => 'required',
+            'slug' => 'required',
+        ]);
+
+        return Department::create($request->all());
+        
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        // display a department from el_salvador database
+        return Department::find($id);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $name
+     * @return \Illuminate\Http\Response
+     */
+
+    public function showByName($name)
+    {
+        // display a department from el_salvador database
+        return Department::where('name', $name)->get();
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
