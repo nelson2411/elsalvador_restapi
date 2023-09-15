db140ac39093bd5ee97d9c855899f2567bab37b0
Nelson Rosales <nelson.rosales83@gmail.com>
2023-09-15 23:13:16 +0200

    feat: styles on views

--- resources/views/departments.blade.php
+++ resources/views/departments.blade.php
@@ -1,12 +1,49 @@
 <x-layout>
         <div class="col py-3">
             <h3>Get all the departments information</h3>
-            <p class="lead p text-break text-wrap" style="width: 80%">
-                An example 2-level sidebar with collasible menu items. The menu functions like an "accordion" where only a single 
-                menu is be open at a time. While the sidebar itself is not toggle-able, it does responsively shrink in width on smaller screens.
+            <p class="text-break text-wrap fs-5" style="width: 80%">
+               Here are the different routes that you can use for retrieving the departments information.
             </p>
-            <ul class="list-unstyled">
-                <li><h5>Responsive</h5> shrinks in width, hides text labels and collapses to icons only on mobile</li>
+            <ul class="list-unstyled mt-5">
+                <li class="my-4">
+                  <h4>Get all the departments</h4>
+                  <p class="fw-bold">GET 
+                    <code>
+                      https://sivarrestapi.herokuapp.com/api/departments
+                  </code>
+                  </p>
+                </li>
+                <li class="my-4">
+                  <h4>Get a department by id</h4>
+                  <p class="fw-bold">GET 
+                    <code class="text-primary" style="font-size: 1.1rem;" >
+                      https://sivarrestapi.herokuapp.com/api/departments/{id}
+                  </code>
+                  </p>
+                </li>
+                <li class="my-4">
+                  <h4 class="pb-2">Get a department by name</h4>
+                   <code class="bg-dark p-2 text-light" >
+                    GET https://sivarrestapi.herokuapp.com/api/departments/search/{name}
+                  </code>
+                 
+                </li>
+                <li class="my-4">
+                  <h4>Get the top-five largest departments</h4>
+                  <p class="fw-bold">GET 
+                    <code class="text-primary" style="font-size: 1.1rem;" >
+                      https://sivarrestapi.herokuapp.com/api/departments/area/top-five
+                  </code>
+                  </p>
+                </li>
+                 <li class="my-4">
+                  <h4>Get a department by id</h4>
+                  <p class="fw-bold">GET 
+                    <code class="text-primary" style="font-size: 1.1rem;" >
+                      https://sivarrestapi.herokuapp.com/api/departments/{id}
+                  </code>
+                  </p>
+                </li>
             </ul>
         </div>
 </x-layout>
